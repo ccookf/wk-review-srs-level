af31a0e8dec465df90b4f38afb58ba8aaa381d39
Chris Cook <ccookf@gmail.com>
2017-06-16 13:49:58 -0400

    Working script. Level names unverified.

---
+++ README.md
@@ -0,0 +1,11 @@
+# WK Show Review SRS Level
+
+A userscript to display the current SRS Level of a review item on WaniKani. For everyone's sanity I've chosen to adopt the [MIT License](https://opensource.org/licenses/MIT). Have fun!
+
+This script is designed on the original work 'WaniKani Show Level' by user 'ruipgpinheiro (LordGravewish)' which was originally licensed under under the [CC BY-NC](https://creativecommons.org/licenses/by-nc/3.0/). To the best of my knowledge the original script is no longer supported or available, hence the reimplimentation. As this was a complete rewrite I've chosen to re
+
+## Credits
+
+Special thanks to rfindley (Robin Findley) whose [Lightning Mode](https://community.wanikani.com/t/UserScript-WaniKani-Lightning-Mode/10201/28) script provided the base for css.
+General design was taken from 'ruipgpinheiro (LordGravewish)' whose original script ['WaniKani Show Level'](https://community.wanikani.com/t/Wanikani-Show-Level-101-Shows-SRS-level-during-reviews-No-longer-supported/3882) is missing to the best of my knowledge, motivating me to create this one.
+Finally, thanks to hoovard's [KunOn+ script](https://community.wanikani.com/t/New-version-of-KunOn-script-WK-Custom-Review-Question-KunOn/7597) for saving me the heading of finding the right storage key.
---
+++ script.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name         WK Show Review SRS Level
+// @namespace    WK_ShowRSRS
+// @version      0.1
+// @description  Prepends an indicator of the SRS level for the current item to the "stats" display.
+// @author       ccookf
+// @copyright    2017+, ccookf
+// @license      MIT; http://opensource.org/licenses/MIT
+// @match        https://www.wanikani.com/review/session*
+// @match        https://www.wanikani.com/review/session*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    addStyle(
+        ".WK_ShowRSRS {display:inline-block; border-radius:3px; padding:0.15em; background-color:#00AAFF; font-weight:bold;}"
+    );
+    var currentItem = $.jStorage.get("currentItem");
+    $("#stats").prepend(formatHTML(currentItem));
+    //monit + update
+    $.jStorage.listenKeyChange("currentItem", () => {
+        currentItem = $.jStorage.get("currentItem");
+        $("#WK_ShowRSRS").html(formatHTML(currentItem));
+    });
+})();
+
+function addStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (head) {
+        style = document.createElement('style');
+        style.setAttribute('type', 'text/css');
+        style.textContent = css;
+        head.appendChild(style);
+        return style;
+    }
+    return null;
+}
+function getLevelName(srs) {
+    switch (srs) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            return "Apprentice";
+        case 5:
+        case 6:
+            return "Guru";
+        case 7:
+            return "Master";
+        case 8:
+            return "Enlightened";
+        default:
+            return "ERR" + srs + "!";
+    }
+}
+function formatHTML(item) {
+    var name = getLevelName(item.srs);
+    return "<div id='WK_ShowRSRS' class='WK_ShowRSRS'>" + name + " (" + item.srs + ")</div>";
+}
